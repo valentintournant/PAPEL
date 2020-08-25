81dd6aa6df1d4089ee459971183001d3a4e2bb1f
jules welcomme <jules.welcomme@gmail.com>
2020-08-25 12:07:41 +0200

    receipt model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_082403) do
+ActiveRecord::Schema.define(version: 2020_08_25_100436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cohorts_on_user_id"
+  end
+
+  create_table "receipts", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.string "store"
+    t.integer "amount"
+    t.text "description"
+    t.string "category_name"
+    t.bigint "user_id", null: false
+    t.bigint "cohort_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cohort_id"], name: "index_receipts_on_cohort_id"
+    t.index ["user_id"], name: "index_receipts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +51,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_082403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cohorts", "users"
+  add_foreign_key "receipts", "cohorts"
+  add_foreign_key "receipts", "users"
 end
