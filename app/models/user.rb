12e87a9fb5b0c57f811c25417cd47679d09682c8
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-08-26 14:01:17 +0200

    beginning of seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
 
   has_many :receipts
   has_many :cohorts, dependent: :destroy
+
+  def total_receipts
+    self.receipts.sum(:amount)
+  end
+
+  def total_receipts_per_category(category_name)
+    self.receipts.where(category_name: category_name).sum(:amount)
+  end
+
+
 end
+
