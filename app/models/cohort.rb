5eccc69ecf228a67a19c4792d612b066d950d4a3
Valentin Tournant <valentin.tournant@gmail.com>
2020-08-26 18:05:50 +0200

    add a member but have to create the controller

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,6 +1,11 @@
 class Cohort < ApplicationRecord
   has_many :receipts
+  has_many :members
 
   validates :title, presence: true
   validates :description, presence: true
+
+  # def owner
+
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :receipts
-  resources :cohorts
+  resources :cohorts do
+    resources :members, only: [:create]
+  end
   resources :profile, only: [:show]
 end
 
