f565368ebadae63bdd69690e1f40fdb20079dec9
Yohan Franquelin <yranquelin@gmail.com>
2020-08-27 10:37:14 +0200

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :receipts
   receipts-belong-group
-  
+
   resources :cohorts do
     resources :members, only: [:create]
   end
