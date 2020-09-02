7b9120371e00acc53bcde676644f07cb58b7e8a3
Valentin Tournant <valentin.tournant@gmail.com>
2020-09-02 17:19:38 +0200

    debugging parser

--- app/services/text_parser_service.rb
+++ app/services/text_parser_service.rb
@@ -31,28 +31,30 @@ class TextParserService
 
   def parse_uniqlo_receipt
 
-    p "=========== text========="
-    p @text
-    puts "it is a Uniqlo ticket"
+
     date = @text.match(/(?:(?:(?:0[1-9]|1\d|2[0-8])\/(?:0[1-9]|1[0-2])|(?:29|30)\/(?:0[13-9]|1[0-2])|31\/(?:0[13578]|1[02]))\/[1-9]\d{3}|29\/02(?:\/[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00))/)[0]
     store = @text.downcase.split('www.')[1].split('.')[0]
     amount = @text.split('EUR')[1].split[0].to_f
-
     address = @text.split(/Uni[gq]lo/)[1].split[0] + " " + @text.split(',')[1].split('TVA')[0].strip
 
-      return {
-        amount: amount,
-        category_name: 'shopping',
-        store: 'Uniqlo',
-        description: nil,
-        date: date,
-        address: address
-      }
+    return {
+      amount: amount,
+      category_name: 'shopping',
+      store: 'Uniqlo',
+      description: nil,
+      date: date,
+      address: address
+    }
   end
 
   def parse_so_good_receipt
 
+    p "=========== text========="
+    p @text
+
     puts "it is a So Good ticket"
+
+    raise
     # TODO: (6) split,strip on the @text
     amount = @text.split('TOTAL TICKET')[1].split('/')[0].strip.to_f
     address = @text.split('EURATECHN')[1].split('49000')[0].strip + " " + @text.split('49000')[1].split('FRANCE')[0].strip
